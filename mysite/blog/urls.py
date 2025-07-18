3e51a3ea74aa9fd983400fd8229badb8a557ff4f
Jobayrul Hasan <jobayerdpi11@gmail.com>
2025-07-18 12:04:44 +0600

    last changes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns = [
     path('blog/', views.blog, name='blog'),
+    path('blog/more1/', views.blog_more1, name='blog_more1'),
+    path('blog/more2/', views.blog_more2, name='blog_more2'),
+    path('blog/more3/', views.blog_more3, name='blog_more3'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def blog(request):
     return render(request, 'blog/index.html')
+
+def blog_more1(request):
+    return render(request, 'blog/blogmore1.html')
+
+def blog_more2(request):
+    return render(request, 'blog/blogmore2.html')
+
+def blog_more3(request):
+    return render(request, 'blog/blogmore3.html')
