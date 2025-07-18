ff63c73c50dc231790a2716ef62da2653856b590
Jobayrul Hasan <jobayerdpi11@gmail.com>
2025-07-19 00:04:45 +0600

    last changes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('blog/more1/', views.blog_more1, name='blog_more1'),
     path('blog/more2/', views.blog_more2, name='blog_more2'),
     path('blog/more3/', views.blog_more3, name='blog_more3'),
+    path('blog/map/', views.map_view, name='map_view'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import plotly.express as px
+import pandas as pd
 
-# Create your views here.
+# Main blog pages
 def blog(request):
     return render(request, 'blog/index.html')
 
@@ -12,3 +14,30 @@ def blog_more2(request):
 
 def blog_more3(request):
     return render(request, 'blog/blogmore3.html')
+
+
+
+# Global map view using Plotly
+def map_view(request):
+    # Sample city data for demonstration
+    df = pd.DataFrame({
+        'City': ['London', 'New York', 'Tokyo'],
+        'Latitude': [51.5074, 40.7128, 35.6762],
+        'Longitude': [-0.1278, -74.0060, 139.6503],
+    })
+
+    # Create the Plotly scatter map
+    fig = px.scatter_geo(
+        df,
+        lat='Latitude',
+        lon='Longitude',
+        text='City',
+        projection='natural earth',
+        title='World Cities'
+    )
+    
+    # Convert Plotly figure to HTML
+    map_html = fig.to_html(full_html=False)
+
+    # Pass it to the template
+    return render(request, 'blog/map.html', {'map_html': map_html})
