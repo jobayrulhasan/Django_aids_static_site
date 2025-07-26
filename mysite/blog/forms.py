22452855792711893d7b2377aa76e1e933513d53
Jobayrul Hasan <jobayerdpi11@gmail.com>
2025-07-26 22:32:25 +0600

    last changes

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -4,9 +4,11 @@ from django import forms
 
 class StudentForm(forms.Form):
     first_name = forms.CharField()
-    last_name = forms.CharField()
+    last_name = forms.CharField(error_messages={'required': 'Please enter your last name.'})
     email = forms.EmailField()
     batch = forms.IntegerField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=20)
+    re_password = forms.CharField(widget=forms.PasswordInput(), min_length=8, max_length=20)
     message = forms.CharField(widget=forms.Textarea)
-    
+    payment = forms.DecimalField(min_value=2500, max_value=10000.0, decimal_places=2)
+    django = forms.BooleanField()
