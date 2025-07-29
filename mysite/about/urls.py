2128a85ad02dc2c4f34ffa077bccbb17927c8334
Jobayrul Hasan <jobayerdpi11@gmail.com>
2025-07-29 11:38:31 +0600

    last changes

--- mysite/about/urls.py
+++ mysite/about/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('student/', views.std_info, name='std_info'),
+    path('userCreate/', views.user_creation_form, name='user_creation_form'),
+    path('userLogin/', views.user_login, name='user_login'),
 ]
--- mysite/about/views.py
+++ mysite/about/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Student
+# django default forms
+from .forms import userCreate
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def about(request):
@@ -9,3 +12,17 @@ def std_info(request):
     # Logic to fetch student information
     sdetails = Student.objects.all()  # Assuming Student model is imported
     return render(request, 'about/student_info.html', {'std':sdetails})
+
+#user creation form
+def user_creation_form(request):
+    if request.method == 'POST':
+        frm = userCreate(request.POST)
+        if frm.is_valid():
+            frm.save()
+    else:
+       frm = userCreate()
+    return render(request, 'about/user_creation.html', {'form': frm})
+
+def user_login(request):
+    frm = AuthenticationForm()
+    return render(request, 'about/user_login.html', {'form': frm})
